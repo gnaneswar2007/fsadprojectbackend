5d1b256b81aa772f4aed1f3030658fd4ee9dbe8a
Mamidala Ganesh Naidu <learnwithgani@gmail.com>
2026-04-08 10:02:43 +0530

    Add comments for getters and setters in LoginRequest
    Added comments for getters and setters

--- src/main/java/com/klu/dto/LoginRequest.java
+++ src/main/java/com/klu/dto/LoginRequest.java
@@ -10,7 +10,7 @@ public class LoginRequest {
         this.email = email;
         this.password = password;
     }
-
+//getters and setters
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
     public String getPassword() { return password; }
