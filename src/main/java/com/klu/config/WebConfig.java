46c9175dd3ff948362e45a49532f7f5637de36ee
gnaneswar2007 <akulagnaneswar19@gmail.com>
2026-04-26 07:28:03 +0530

    fsadproject

--- src/main/java/com/klu/config/WebConfig.java
+++ src/main/java/com/klu/config/WebConfig.java
@@ -13,10 +13,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://fsadproject-indol.vercel.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");      
             }
         };
     }
